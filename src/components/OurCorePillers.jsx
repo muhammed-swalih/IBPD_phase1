c27b4a98ccf1fe65180e8adfa7117a2807f5caa8
muhammed-swalih <swalihofficial7@gmail.com>
2024-03-14 04:46:27 +0530

    sorry shiijas again

--- src/components/OurCorePillers.jsx
+++ src/components/OurCorePillers.jsx
@@ -16,7 +16,7 @@ function OurCorePillers() {
       <div className=" px-20 flex flex-col gap-8">
         <div className=" flex flex-col gap-1">
           <h1 className=" text-xl font-semibold">Comprehensive Learning</h1>
-          <h1 className=" text-justify w-[1000px]">
+          <h1 className=" text-justify w-[830px]">
             IBPD believes in going beyond the conventional, offering a holistic
             approach to professional development. Our programs encompass not
             only technical proficiency but also leadership, communication, and
@@ -25,7 +25,7 @@ function OurCorePillers() {
         </div>
         <div className=" flex flex-col gap-1">
           <h1 className=" text-xl font-semibold">Guided by Experts</h1>
-          <h1 className=" text-justify w-[1000px]">
+          <h1 className=" text-justify w-[830px]">
             When you choose IBPD, you gain access to a cadre of industry experts
             and seasoned professionals. Our commitment to expert guidance
             ensures that your journey is marked by invaluable insights,
@@ -34,7 +34,7 @@ function OurCorePillers() {
         </div>
         <div className=" flex flex-col gap-1">
           <h1 className=" text-xl font-semibold">Cutting-edge Resources</h1>
-          <h1 className="text-justify w-[1000px]">
+          <h1 className="text-justify w-[830px]">
             In a rapidly evolving professional landscape, staying ahead is not
             an option; it's a necessity. IBPD provides cutting-edge resources,
             keeping you updated on the latest industry trends, technologies, and
@@ -43,7 +43,7 @@ function OurCorePillers() {
         </div>
         <div className=" flex flex-col gap-1">
           <h1 className=" text-xl font-semibold">Thriving Community</h1>
-          <h1 className="text-justify w-[1000px]">
+          <h1 className="text-justify w-[830px]">
             Join a community of like-minded individuals who share your passion
             for continuous improvement. IBPD is not just a platform; it's a
             supportive network where collaboration and shared learning are
@@ -52,7 +52,7 @@ function OurCorePillers() {
         </div>
         <div className=" flex flex-col gap-1">
           <h1 className=" text-xl font-semibold">Better Recognition</h1>
-          <h1 className="text-justify w-[1000px]">
+          <h1 className="text-justify w-[830px]">
             IBPD recognizes and celebrates your unique talents and
             contributions. Our commitment to 'Better Recognition' ensures that
             your efforts are not just seen but appreciated, setting you on a
@@ -64,7 +64,7 @@ function OurCorePillers() {
           <h1 className=" text-xl font-semibold">
             Strategic Investment in Your Future
           </h1>
-          <h1 className="text-justify w-[1000px]">
+          <h1 className="text-justify w-[830px]">
             Choosing IBPD is not just a decision; it's a strategic investment in
             your future. Our programs and resources are designed for sustained
             and amplified achievements throughout your professional journey,
@@ -73,12 +73,29 @@ function OurCorePillers() {
         </div>
         <div className=" flex flex-col gap-1">
           <h1 className=" text-xl font-semibold">Excellence as Standard</h1>
-          <h1 className="text-justify w-[1000px]">
+          <h1 className="text-justify w-[830px]">
             At IBPD, we set a high standard for excellence. Our focus on
             excellence permeates every aspect of our offerings, instilling a
             culture of continuous improvement and ensuring that you are
             well-prepared to excel in your respective field.
           </h1>
+        </div>
+      </div>
+      <div className=" mx-20">
+        <div className=" w-[1000px] py-8 px-10 bg-black rounded-xl">
+          <div className=" flex flex-col gap-3">
+            <h1 className=" text-3xl text-white font-semibold">
+              Join IBPD and Redefine Your Professional Journey.
+            </h1>
+            <hr className=" w-[830px] border-gray-600" />
+            <h1 className=" w-[830px] text-white">
+              Embark on a transformative experience with IBPD – where expertise
+              meets aspiration, crafting futures of unparalleled success. Ignite
+              your career, illuminate your future, and let IBPD be your gateway
+              to unmatched professional development. Your success story starts
+              here.
+            </h1>
+          </div>
         </div>
       </div>
     </div>
